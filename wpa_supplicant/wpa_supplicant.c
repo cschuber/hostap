cschuber/hostap

commit 9aa9cd3b645e3e581122c3ca3f99fc449c4b02f7

EPPKE: Advertise RSNXE capabilities in (Re)Association Request frame
Add support for including the RSNXE bits for KEK_IN_PASN and
ASSOCIATION_FRAME_ENCRYPTION in (Re)Association Request frame for EPPKE
authentication. Set these bits only if the AP indicates support for this
to work around issues with some deployed APs having problems in parsing
RSNXE with multiple octets of payload.

Signed-off-by: Ainy Kumari <ainy.kumari@oss.qualcomm.com>

wpa_supplicant/wpa_supplicant.c
@@ -454,6 +454,7 @@ void wpa_supplicant_set_non_wpa_policy(struct wpa_supplicant *wpa_s,
 	wpa_sm_set_param(wpa_s->wpa, WPA_PARAM_MGMT_GROUP,
 			 wpa_s->mgmt_group_cipher);
 	wpa_sm_set_param(wpa_s->wpa, WPA_PARAM_SSID_PROTECTION, 0);
+	wpa_sm_set_param(wpa_s->wpa, WPA_PARAM_ASSOC_ENC, 0);
 
 	pmksa_cache_clear_current(wpa_s->wpa);
 	os_memset(&mlo, 0, sizeof(mlo));
@@ -2288,6 +2289,27 @@ int wpa_supplicant_set_suites(struct wpa_supplicant *wpa_s,
 	} else {
 		wpa_sm_set_param(wpa_s->wpa, WPA_PARAM_SSID_PROTECTION, false);
 	}
+
+#ifdef CONFIG_ENC_ASSOC
+	if (ssid->ssid_protection && proto == WPA_PROTO_RSN &&
+	    (wpa_s->drv_flags2 &
+	     WPA_DRIVER_FLAGS2_ASSOCIATION_FRAME_ENCRYPTION)) {
+		bool assoc_enc;
+
+		/* Enable association frame encryption based on the AP
+		 * advertising support for it to avoid potential
+		 * interoperability issues with incorrect AP behavior if we
+		 * were to send an "unexpected" RSNXE with multiple octets of
+		 * payload. */
+		assoc_enc = ieee802_11_rsnx_capab(
+			bss_rsnx, WLAN_RSNX_CAPAB_ASSOC_FRAME_ENCRYPTION);
+		if (!skip_default_rsne)
+			wpa_sm_set_param(wpa_s->wpa, WPA_PARAM_ASSOC_ENC,
+					 assoc_enc);
+	} else {
+		wpa_sm_set_param(wpa_s->wpa, WPA_PARAM_ASSOC_ENC, false);
+	}
+#endif /* CONFIG_ENC_ASSOC */
 
 	wpa_sm_set_param(wpa_s->wpa, WPA_PARAM_SPP_AMSDU,
 			 (wpa_s->drv_flags2 & WPA_DRIVER_FLAGS2_SPP_AMSDU) &&

src/rsn_supp/wpa.c
@@ -5149,6 +5149,9 @@ int wpa_sm_set_param(struct wpa_sm *sm, enum wpa_sm_conf_params param,
 	case WPA_PARAM_SAE_PW_ID_CHANGE:
 		sm->sae_pw_id_change = !!value;
 		break;
+	case WPA_PARAM_ASSOC_ENC:
+		sm->assoc_encryption = !!value;
+		break;
 	default:
 		break;
 	}

src/rsn_supp/wpa.h
@@ -145,6 +145,7 @@ enum wpa_sm_conf_params {
 	WPA_PARAM_URNM_MFPR,
 	WPA_PARAM_URNM_MFPR_X20,
 	WPA_PARAM_SAE_PW_ID_CHANGE,
+	WPA_PARAM_ASSOC_ENC,
 };
 
 enum wpa_rsn_override {

src/rsn_supp/wpa_i.h
@@ -117,6 +117,7 @@ struct wpa_sm {
 	unsigned int ssid_protection:1;
 	unsigned int spp_amsdu:1;
 	unsigned int sae_pw_id_change:1;
+	unsigned int assoc_encryption:1;
 
 	u8 *assoc_wpa_ie; /* Own WPA/RSN IE from (Re)AssocReq */
 	size_t assoc_wpa_ie_len;

src/rsn_supp/wpa_ie.c
@@ -328,6 +328,11 @@ int wpa_gen_rsnxe(struct wpa_sm *sm, u8 *rsnxe, size_t rsnxe_len)
 		capab |= BIT(WLAN_RSNX_CAPAB_SPP_A_MSDU);
 	if (sm->sae_pw_id_change)
 		capab |= BIT_ULL(WLAN_RSNX_CAPAB_SAE_PW_ID_CHANGE);
+#ifdef CONFIG_ENC_ASSOC
+	if (sm->assoc_encryption)
+		capab |= BIT(WLAN_RSNX_CAPAB_ASSOC_FRAME_ENCRYPTION) |
+			BIT(WLAN_RSNX_CAPAB_KEK_IN_PASN);
+#endif /* CONFIG_ENC_ASSOC */
 
 	if (!capab)
 		return 0; /* no supported extended RSN capabilities */

commit 1231aacee90d1473a65baea4505e90915fefbda5

EPPKE: Remove incorrect ssid_protection check for assoc frame encryption
Commit 9aa9cd3b645e ("EPPKE: Advertise RSNXE capabilities in
(Re)Association Request frame") incorrectly reuses the ssid_protection
configuration parameter to control (re)association frame encryption due
to a copy-paste issue in using the previous code as a template.

SSID protection and (re)association frame encryption serve different
purposes and should not be coupled together. Association frame
encryption should be controlled independently and not depend on the
ssid_protection configuration flag.

Remove the ssid_protection configuration parameter that incorrectly
checks ssid_protection to enable WPA_PARAM_ASSOC_ENC.

Fixes: 9aa9cd3b645e ("EPPKE: Advertise RSNXE capabilities in (Re)Association Request frame")
Signed-off-by: Kavita Kavita <kavita.kavita@oss.qualcomm.com>

wpa_supplicant/wpa_supplicant.c
@@ -2291,7 +2291,7 @@ int wpa_supplicant_set_suites(struct wpa_supplicant *wpa_s,
 	}
 
 #ifdef CONFIG_ENC_ASSOC
-	if (ssid->ssid_protection && proto == WPA_PROTO_RSN &&
+	if (proto == WPA_PROTO_RSN &&
 	    (wpa_s->drv_flags2 &
 	     WPA_DRIVER_FLAGS2_ASSOCIATION_FRAME_ENCRYPTION)) {
 		bool assoc_enc;