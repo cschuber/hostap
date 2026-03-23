cschuber/hostap

commit e6dbaa673f3941cc334a879ae553b2076a81acdf

SAE: Limit PMKSA flushing on failure to the current BSS
Instead of flushing all PMKSA cache entries for the full ESS (i.e., all
BSSs in it), flush the PMKSA cache entry only for the BSS that rejected
an attempt to use PMKSA caching with SAE.

Signed-off-by: Jouni Malinen <jouni.malinen@oss.qualcomm.com>

src/rsn_supp/pmksa_cache.c
@@ -374,7 +374,7 @@ pmksa_cache_add_entry(struct rsn_pmksa_cache *pmksa,
 			if (!pos->opportunistic)
 				pmksa_cache_flush(pmksa, entry->network_ctx,
 						  pos->pmk, pos->pmk_len,
-						  false);
+						  false, NULL);
 			pmksa_cache_free_entry(pmksa, pos, PMKSA_REPLACE);
 			break;
 		}
@@ -449,9 +449,11 @@ pmksa_cache_add_entry(struct rsn_pmksa_cache *pmksa,
  * @pmk_len: PMK length
  * @external_only: Flush only PMKSA cache entries configured by external
  * applications
+ * @addr: AA to match or %NULL to match all addresses
  */
 void pmksa_cache_flush(struct rsn_pmksa_cache *pmksa, void *network_ctx,
-		       const u8 *pmk, size_t pmk_len, bool external_only)
+		       const u8 *pmk, size_t pmk_len, bool external_only,
+		       const u8 *addr)
 {
 	struct rsn_pmksa_cache_entry *entry, *prev = NULL, *tmp;
 	int removed = 0;

src/pasn/pasn_initiator.c
@@ -83,7 +83,7 @@ int pasn_initiator_pmksa_cache_get(struct rsn_pmksa_cache *pmksa,
 
 void pasn_initiator_pmksa_cache_flush(struct rsn_pmksa_cache *pmksa)
 {
-	return pmksa_cache_flush(pmksa, NULL, NULL, 0, false);
+	return pmksa_cache_flush(pmksa, NULL, NULL, 0, false, NULL);
 }
 
 

src/rsn_supp/pmksa_cache.h
@@ -99,7 +99,8 @@ struct rsn_pmksa_cache_entry *
 pmksa_cache_get_opportunistic(struct rsn_pmksa_cache *pmksa,
 			      void *network_ctx, const u8 *aa, int akmp);
 void pmksa_cache_flush(struct rsn_pmksa_cache *pmksa, void *network_ctx,
-		       const u8 *pmk, size_t pmk_len, bool external_only);
+		       const u8 *pmk, size_t pmk_len, bool external_only,
+		       const u8 *addr);
 void pmksa_cache_remove(struct rsn_pmksa_cache *pmksa,
 			struct rsn_pmksa_cache_entry *entry);
 void pmksa_cache_reconfig(struct rsn_pmksa_cache *pmksa);

src/rsn_supp/wpa.c
@@ -5911,13 +5911,20 @@ void wpa_sm_update_replay_ctr(struct wpa_sm *sm, const u8 *replay_ctr)
 
 void wpa_sm_pmksa_cache_flush(struct wpa_sm *sm, void *network_ctx)
 {
-	pmksa_cache_flush(sm->pmksa, network_ctx, NULL, 0, false);
+	pmksa_cache_flush(sm->pmksa, network_ctx, NULL, 0, false, NULL);
+}
+
+
+void wpa_sm_pmksa_cache_flush_addr(struct wpa_sm *sm, void *network_ctx,
+				   const u8 *addr)
+{
+	pmksa_cache_flush(sm->pmksa, network_ctx, NULL, 0, false, addr);
 }
 
 
 void wpa_sm_external_pmksa_cache_flush(struct wpa_sm *sm, void *network_ctx)
 {
-	pmksa_cache_flush(sm->pmksa, network_ctx, NULL, 0, true);
+	pmksa_cache_flush(sm->pmksa, network_ctx, NULL, 0, true, NULL);
 }
 
 

src/rsn_supp/wpa.h
@@ -273,6 +273,8 @@ int wpa_sm_has_ptk_installed(struct wpa_sm *sm);
 void wpa_sm_update_replay_ctr(struct wpa_sm *sm, const u8 *replay_ctr);
 
 void wpa_sm_pmksa_cache_flush(struct wpa_sm *sm, void *network_ctx);
+void wpa_sm_pmksa_cache_flush_addr(struct wpa_sm *sm, void *network_ctx,
+				   const u8 *addr);
 void wpa_sm_external_pmksa_cache_flush(struct wpa_sm *sm, void *network_ctx);
 
 int wpa_sm_get_p2p_ip_addr(struct wpa_sm *sm, u8 *buf);
@@ -497,6 +499,12 @@ static inline void wpa_sm_update_replay_ctr(struct wpa_sm *sm,
 
 static inline void wpa_sm_external_pmksa_cache_flush(struct wpa_sm *sm,
 						     void *network_ctx)
+{
+}
+
+static inline void wpa_sm_external_pmksa_cache_flush_addr(struct wpa_sm *sm,
+							  void *network_ctx,
+							  const u8 *addr)
 {
 }
 

wpa_supplicant/sme.c
@@ -3389,12 +3389,22 @@ void sme_event_assoc_reject(struct wpa_supplicant *wpa_s,
 #ifdef CONFIG_SAE
 	if (wpa_s->sme.sae_pmksa_caching && wpa_s->current_ssid &&
 	    wpa_key_mgmt_sae(wpa_s->current_ssid->key_mgmt)) {
+		struct wpa_bss *bss = wpa_s->current_bss;
+		const u8 *aa;
+
 		wpa_dbg(wpa_s, MSG_DEBUG,
 			"PMKSA caching attempt rejected - drop PMKSA cache entry and fall back to SAE authentication");
 		wpa_sm_aborted_cached(wpa_s->wpa);
-		wpa_sm_pmksa_cache_flush(wpa_s->wpa, wpa_s->current_ssid);
+		if (wpa_s->valid_links)
+			aa = wpa_s->ap_mld_addr;
+		else if (bss)
+			aa = bss->bssid;
+		else
+			aa = NULL;
+
+		wpa_sm_pmksa_cache_flush_addr(wpa_s->wpa, wpa_s->current_ssid,
+					      aa);
 		if (wpa_s->current_bss) {
-			struct wpa_bss *bss = wpa_s->current_bss;
 			struct wpa_ssid *ssid = wpa_s->current_ssid;
 
 			wpa_drv_deauthenticate(wpa_s, bssid,

commit f54565c6293d03bf7da5b7c4af496a62c51f0aaf

RSN: Fix pmksa_cache_flush prototype mismatch in non-IEEE8021X_EAPOL case
Fix compilation error caused by conflicting declarations of
pmksa_cache_flush() when IEEE8021X_EAPOL is not enabled or
CONFIG_NO_WPA is set. The function definition in pmksa_cache.c
was missing the addr parameter present in the header declaration
for this build configuration.

Align the implementation with the header by adding the missing
parameter.

Fixes: e6dbaa673f39 ("SAE: Limit PMKSA flushing on failure to the current BSS")
Signed-off-by: Nick Hainke <vincent@systemli.org>

src/rsn_supp/pmksa_cache.c
@@ -955,7 +955,8 @@ int pmksa_cache_set_current(struct wpa_sm *sm, const u8 *pmkid, const u8 *bssid,
 
 
 void pmksa_cache_flush(struct rsn_pmksa_cache *pmksa, void *network_ctx,
-		       const u8 *pmk, size_t pmk_len, bool external_only)
+		       const u8 *pmk, size_t pmk_len, bool external_only,
+		       const u8 *addr)
 {
 }
 